c5ba1afa9721edaf39051c29d09eabbbff55a3f1
Merretby <mouhamaderretby2002@gmail.com>
2024-12-12 10:14:36 +0100

    done

--- Cpp1/ex04/main.cpp
+++ Cpp1/ex04/main.cpp
@@ -15,17 +15,37 @@ int main(int ac, char **av)
         std::string s1 = av[2];
         std::string s2 = av[3];
         std::ifstream input(filename);
-        if (!input)
+        if (s1.empty() || s2.empty())
+        {
+            std::cerr << "Error: s1 and s2 must not be empty" << std::endl;
+            return 1;
+        }
+        if (!input.is_open())
         {
             std::cerr << "Error: could not open file" << std::endl;
             return 1;
         }
         std::string output = filename + ".replace";
         std::ofstream replace(output);
-        if (!replace)
+        if (!replace.is_open())
         {
             std::cerr << "Error: could not create file" << std::endl;
             return 1;
         }
+        std::string line;
+        while (std::getline(input, line))
+        {
+            size_t pos = 0;
+            while ((pos = line.find(s1, pos)) != std::string::npos)
+            {
+                line.erase(pos, s1.length());
+                line.insert(pos, s2);
+                pos += s2.length();
+            }
+            replace << line << "\n";
+        }
+        input.close();
+        replace.close();
+        return 0;
     }
 }
